4e6518d83e0295902f21288179ec695de2c6c747
alex <smartmanage715@gmail.com>
2022-11-04 02:47:48 +0900

    Added readme

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-phpinfo();
-?>
+
+
+// Define minimum supported PHP version
+define('APPSVAN_PHP', '7.3.0');
+define('LARAVEL_START', microtime(true));
+
+// Check PHP version
+if (version_compare(PHP_VERSION, APPSVAN_PHP, '<')) {
+    die('Your host needs to use PHP ' . APPSVAN_PHP . ' or higher to run Akaunting.<br> PHP VERSION : '.PHP_VERSION);
+}
+
+// Register the auto-loader
+require __DIR__.'/vendor/autoload.php';
+
+// Load the app
+$app = require_once(__DIR__.'/bootstrap/app.php');
+
+// Run the app
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
